f47e991697a0f99884a20b641756a00cbc14bb2b
Sam Fredrickson <kinghajj@gmail.com>
2013-04-11 01:40:10 -0700

    First working prototype!

--- web.js
+++ web.js
@@ -1,12 +1,50 @@
-var express = require('express');
+var spawn   = require('child_process').spawn,
+    fs      = require('fs'),
+    path    = require('path'),
+    app     = require('http').createServer(http_handler),
+    io      = require('socket.io').listen(app),
+    url     = require('url');
 
-var app = express.createServer(express.logger());
+var api_url = 'http://ltc.kattare.com/api.php?api_key=64b0fea666d47d5dd5ec5d609b0ae5925200626b495359c52ccdc18eb7ff3369';
+var delay   = 60000;
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// I've had no luck using the native Node ways to fetch from HTTP, so, fuck it,
+// just use wget. Always reliable!
+function wget(url) {
+    return spawn('wget', ['-q', '-O', '-', url]);
+}
 
+// periodically fetch stats and emit them to clients
+function socket_handler(socket) {
+    wget(api_url).stdout.on('data', function(data) {
+        var pool_stats = JSON.parse(data);
+        pool_stats.now = (new Date()).getTime();
+        socket.emit('news', pool_stats);
+        setTimeout(socket_handler, delay, socket);
+    });
+}
+io.sockets.on('connection', socket_handler);
+
+// HTTP server main logic
+function http_handler(req, res) {
+    req_url = url.parse(req.url);
+    // redirect to main page
+    if(req_url.pathname === '/') {
+        req_url.pathname = '/lightningthieves.html';
+    }
+    // sanitize the path
+    req_url.pathname = path.normalize(req_url.pathname);
+    // try to load the file and serve it
+    fs.readFile(__dirname + req_url.pathname, function(err, data) {
+        if(err) {
+            res.writeHead(500);
+            return res.end('Error loading ' + req_url.pathname);
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}
+
+// start listening
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port);
