5fa9b2d66f631259fac70a3052309cbb1e91ad69
Sam Fredrickson <kinghajj@gmail.com>
2013-05-22 17:02:44 -0700

    Major UI change.

--- js/lightningthieves.js
+++ js/lightningthieves.js
@@ -1,11 +1,9 @@
 var stats;
+var converter;
 
 function updatePage() {
   var then   = new Date(stats.now);
   var now    = new Date();
-  var status = $("#status");
-  status.text('Last update received at ' + now.toLocaleTimeString() +
-              '; current as of ' + then.toLocaleTimeString());
 
   // check if workers running, possibly alert user
   var running = true;
@@ -24,41 +22,68 @@ function updatePage() {
   var last_ltcusd = stats.btce_ltcusd.ticker.last  * 1;
   var last_btcusd = stats.mtgox_btcusd.data.last.value * 1;
 
-  var confirmed_rewards_ltc = stats.ktr.confirmed_rewards * 1;
-  var estimated_rewards_ltc = stats.ktr.estimated_rewards * 1;
-  var payout_history_ltc    = stats.ktr.payout_history    * 1;
-  var confirmed_rewards_btc = confirmed_rewards_ltc       * last_ltcbtc;
-  var estimated_rewards_btc = estimated_rewards_ltc       * last_ltcbtc;
-  var payout_history_btc    = payout_history_ltc          * last_ltcbtc;
-  var confirmed_rewards_usd = confirmed_rewards_ltc       * last_ltcusd;
-  var estimated_rewards_usd = estimated_rewards_ltc       * last_ltcusd;
-  var payout_history_usd    = payout_history_ltc          * last_ltcusd;
+  $("#btce_ltcbtc").text(last_ltcbtc);
+  $("#btce_ltcusd").text(last_ltcusd);
+  $("#mtgox_btcusd").text(last_btcusd);
 
-  $("#hashrate").text(stats.ktr.hashrate);
-  $("#last_ltcbtc").text(last_ltcbtc);
-  $("#last_ltcusd").text(last_ltcusd);
-  $("#last_btcusd").text(last_btcusd);
-
-  $("#confirmed_rewards_ltc").text(confirmed_rewards_ltc);
-  $("#estimated_rewards_ltc").text(estimated_rewards_ltc);
-  $("#payout_history_ltc")   .text(payout_history_ltc);
-  $("#confirmed_rewards_btc").text(confirmed_rewards_btc);
-  $("#estimated_rewards_btc").text(estimated_rewards_btc);
-  $("#payout_history_btc")   .text(payout_history_btc);
-  $("#confirmed_rewards_usd").text(confirmed_rewards_usd);
-  $("#estimated_rewards_usd").text(estimated_rewards_usd);
-  $("#payout_history_usd")   .text(payout_history_usd);
+  $("#hash_rate").text(stats.ktr.hashrate);
+  $("#confirmed_rewards").text(converter(stats.ktr.confirmed_rewards));
+  $("#estimated_rewards").text(converter(stats.ktr.estimated_rewards));
+  $("#payout_history").text(converter(stats.ktr.payout_history));
 }
 
 $(function() {
-  $(".alert").hide();
-  var status = $("#status");
-  status.text('Connecting to news stream...');
-  var socket = io.connect();
-  status.text('Waiting to receive first update...');
+  /* UI init */
 
+  // currency button click handlers
+  $("#btn_ltcltc").click(function() {
+    converter = function(ltc_amount) {
+      return ltc_amount;
+    };
+    $(".cur-currency").text("LTC");
+    if(stats)
+      updatePage();
+  });
+  $("#btn_btce_ltcbtc").click(function() {
+    converter = function(ltc_amount) {
+      return ltc_amount * stats.btce_ltcbtc.ticker.last;
+    };
+    $(".cur-currency").text("BTCe LTCBTC");
+    if(stats)
+      updatePage();
+  });
+  $("#btn_btce_ltcusd").click(function() {
+    converter = function(ltc_amount) {
+      return ltc_amount * stats.btce_ltcusd.ticker.last;
+    };
+    $(".cur-currency").text("BTCe LTCUSD");
+    if(stats)
+      updatePage();
+  });
+  $("#btn_mtgox_btcusd").click(function() {
+    converter = function(ltc_amount) {
+      return ltc_amount * stats.btce_ltcbtc.ticker.last * stats.mtgox_btcusd.data.last.value;
+    };
+    $(".cur-currency").text("BTCe LTCBTC -> MtGox BTCUSD");
+    if(stats)
+      updatePage();
+  });
+
+  // trigger this to be the default.
+  $("#btn_ltcltc").click();
+
+  // currency button tooltips
+  $(".exchange-tooltip").tooltip({
+    title: "Click to convert the entries in the next table to this currency!",
+    placement: 'bottom'
+  });
+
+  // hide all alerts until we get some news
+  $(".alert").hide();
+
+  var socket = io.connect();
   socket.on('news', function(news) {
     stats = news;
-    //updatePage();
+    updatePage();
   });
 });
